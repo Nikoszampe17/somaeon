f223de588d0c8717434a3e13e2c56250d682afab
Nikoszampe17 <zampetakisnikos@gmail.com>
2025-07-22 11:41:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// Wait until DOM is ready
+
+// Shrink header on scroll
 document.addEventListener('DOMContentLoaded', () => {
   const header = document.querySelector('.main-header');
 
@@ -10,3 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+ // Hamburger navMenu
+const hamburger = document.getElementById('hamburger');
+const navLeft = document.querySelector('.nav-left');
+const navRight = document.querySelector('.nav-right');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+  hamburger.setAttribute('aria-expanded', !expanded);
+
+  navLeft.classList.toggle('active');
+  navRight.classList.toggle('active');
+});
+
+// Ham menu closure after tapping an option -- Mobile UX
+document.querySelectorAll('.nav-left a, .nav-right a').forEach(link => {
+  link.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    hamburger.setAttribute('aria-expanded', 'false');
+    navLeft.classList.remove('active');
+    navRight.classList.remove('active');
+  });
+});
+
+//logo home button ΝΑ ΤΟ ΚΑΝΩ ΣΤΟ ΤΕΛΟΣ
+
